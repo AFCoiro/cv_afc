04905c27380ea91146b0f106b6bc0913f9d336c8
AFCoiro <95722600+AFCoiro@users.noreply.github.com>
2023-03-03 10:49:52 -0300

    Update conocimientos.js

--- js/conocimientos.js
+++ js/conocimientos.js
@@ -1,17 +1,18 @@
 //Array de las direcciones de imagenes para poder mostrarlas mediante fnKnow, que se ejecuta cuando se selecciona el li "conocimientos"
 
 const arrayImg = [
-    "./img/skills/React.png",
-    "./img/skills/html5.png",
-    "./img/skills/javascript.png",
-    "./img/skills/css3.png",
-    "./img/skills/Sass.png",
-    "./img/skills/git.png",
-    "./img/skills/JSON.png",
-    "./img/skills/Bootstrap.png",
-    "./img/skills/material_design.png",
-    "./img/skills/Firebase.png",
-    "./img/skills/FileZilla.png"
+    "React",
+    "html5",
+    "javascript",
+    "css3",
+    "Sass",
+    "git",
+    "JSON",
+    "Bootstrap",
+    "material_design",
+    "Firebase",
+    "FileZilla",
+    "visual_studio"
   ];
 
 function fnKnow() {
@@ -21,7 +22,7 @@ function fnKnow() {
   arrayImg.forEach( mImg =>{
     const divSubcnt = document.createElement('div');
     divSubcnt.setAttribute('class','divSubcnt1');
-    divSubcnt.innerHTML += `<img src="${mImg}" alt="${mImg}" class="imgKno">`;
+    divSubcnt.innerHTML += `<img src="./img/skills/${mImg}.png" alt="${mImg}" class="imgKno">`;
     divCont1.append(divSubcnt);
     }
   )
